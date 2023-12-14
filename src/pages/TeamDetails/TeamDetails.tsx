582e4206910ba8814885bc8eedbdcdff039fac66
agusscript <agus.sanchez.240@gmail.com>
2023-12-14 11:07:37 -0300

    Add team details page styles

--- src/pages/TeamDetails/TeamDetails.tsx
+++ src/pages/TeamDetails/TeamDetails.tsx
@@ -15,11 +15,10 @@ function TeamDetails(): JSX.Element {
   return (
     <section className="team-details-section">
       {!loading ? (
-        <div>
-          <h1>Team Details</h1>
-          <h2 className="team-name" data-name={teamDetails.name}>
+        <div className="details-wrapper">
+          <h1 className="team-name" data-name={teamDetails.name}>
             {teamDetails.name}
-          </h2>
+          </h1>
           <img
             className="team-image"
             src={teamDetails.crestUrl ? teamDetails.crestUrl : imagePlaceholder}
@@ -28,16 +27,38 @@ function TeamDetails(): JSX.Element {
               e.currentTarget.src = imagePlaceholder;
             }}
           />
-          <p className="team-tla">{teamDetails.tla}</p>
-          <p className="team-country">{teamDetails.area.name}</p>
-          <p>{teamDetails.address}</p>
-          <p className="team-venue">Stadium {teamDetails.venue}</p>
-          <p className="team-founded">Founded in {teamDetails.founded}</p>
-          <p className="team-colors">Colors: {teamDetails.clubColors}</p>
-          <p>Email: {teamDetails.email}</p>
-          <p>Website: {teamDetails.website}</p>
-          <p>Phone: {teamDetails.phone}</p>
-          <p>Last Updated: {teamDetails.lastUpdated}</p>
+          <p className="team-tla">
+            <span className="label-detail">Abbreviation</span>
+            {teamDetails.tla}
+          </p>
+          <p className="team-country">
+            <span className="label-detail">Area</span>
+            {teamDetails.area.name}
+          </p>
+          <p className="team-address">
+            <span className="label-detail">Address</span>
+            {teamDetails.address}
+          </p>
+          <p className="team-venue">
+            <span className="label-detail">Stadium</span> {teamDetails.venue}
+          </p>
+          <p className="team-founded">
+            <span className="label-detail">Founded in</span> {teamDetails.founded}
+          </p>
+          <p className="team-colors">
+            <span className="label-detail">Colors</span>
+            {teamDetails.clubColors}
+          </p>
+          <p className="team-email">
+            <span className="label-detail">Email</span> {teamDetails.email}
+          </p>
+          <p className="team-website">
+            <span className="label-detail">Website</span>{" "}
+            <a href={teamDetails.website} target="_blank">{teamDetails.website}</a>
+          </p>
+          <p className="team-phone">
+            <span className="label-detail">Phone</span> {teamDetails.phone}
+          </p>
         </div>
       ) : (
         <Loader />
